1f93a7b3230e6c1774931a1ac55c10136bff194e
root <root@discourse.steffenfoerster.com>
2025-02-01 13:32:51 +0000

    Moved site settings to config/settings.yml

--- plugin.rb
+++ plugin.rb
@@ -6,57 +6,7 @@
 
 enabled_site_setting :subscription_limits_enabled
 
-# name: discourse-subscription-limits
-# about: Limits posts and image uploads in the Image Critique category based on subscription tier.
-# version: 1.0
-# authors: Your Name
-# url: https://your-website.com
-
-enabled_site_setting :subscription_limits_enabled
-
 after_initialize do
-  SiteSetting::Definition.create!(
-    name: "basic_max_posts",
-    data_type: SiteSetting::TypeInteger,
-    default: 1,
-    min: 0
-  )
-
-  SiteSetting::Definition.create!(
-    name: "premium_max_posts",
-    data_type: SiteSetting::TypeInteger,
-    default: 3,
-    min: 0
-  )
-
-  SiteSetting::Definition.create!(
-    name: "elite_max_posts",
-    data_type: SiteSetting::TypeInteger,
-    default: 9999,
-    min: 0
-  )
-
-  SiteSetting::Definition.create!(
-    name: "basic_max_images",
-    data_type: SiteSetting::TypeInteger,
-    default: 3,
-    min: 0
-  )
-
-  SiteSetting::Definition.create!(
-    name: "premium_max_images",
-    data_type: SiteSetting::TypeInteger,
-    default: 5,
-    min: 0
-  )
-
-  SiteSetting::Definition.create!(
-    name: "elite_max_images",
-    data_type: SiteSetting::TypeInteger,
-    default: 9999,
-    min: 0
-  )
-
   module ::SubscriptionLimits
     class PostValidator
       def self.can_post?(user, category)
@@ -85,37 +35,3 @@ after_initialize do
     end
   end
 end
-
-after_initialize do
-  module ::SubscriptionLimits
-    class UploadValidator
-      def self.max_uploads(user)
-        case user.primary_group_name
-        when "Basic" then SiteSetting.basic_max_images
-        when "Premium" then SiteSetting.premium_max_images
-        else SiteSetting.elite_max_images # Default for Elite
-        end
-      end
-    end
-  end
-
-  DiscourseEvent.on(:upload_created) do |upload, post|
-    max_images = SubscriptionLimits::UploadValidator.max_uploads(upload.user)
-    if post.uploads.count > max_images
-      raise Discourse::InvalidAccess, "You've reached your image upload limit for this post."
-    end
-  end
-end
-
-after_initialize do
-  module ::SubscriptionLimits
-    class ResetLimitsJob < ::Jobs::Scheduled
-      every 1.month
-
-      def execute(args)
-        DB.exec("UPDATE users SET monthly_posts_count = 0")
-      end
-    end
-  end
-end
-
